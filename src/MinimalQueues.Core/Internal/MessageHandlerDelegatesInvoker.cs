43cd577e6a8c28ebeebf877b13f116a81fafb220
cesarturo <8376819+cesarturo@users.noreply.github.com>
2023-02-07 23:31:48 -0500

    Reduce allocations when invoking MessageHandlerDelegates

---
+++ src/MinimalQueues.Core/Internal/MessageHandlerDelegatesInvoker.cs
@@ -0,0 +1,29 @@
+﻿namespace MinimalQueues.Core
+{
+    internal class MessageHandlerDelegatesInvoker
+    {
+        private readonly List<MessageHandlerDelegate> _messageHandlerDelegates;
+        private readonly CancellationToken _token;
+        private int _i;
+        private IServiceProvider? _provider;
+        internal MessageHandlerDelegatesInvoker(List<MessageHandlerDelegate> messageHandlerDelegates, CancellationToken token)
+        {
+            _messageHandlerDelegates = messageHandlerDelegates;
+            _token = token;
+        }
+        public Task Handle(IMessage message, IServiceProvider provider)
+        {
+            _provider = provider;
+            return HandleN(message, provider);
+        }
+        private Task HandleN(IMessage message, IServiceProvider provider)
+        {
+            var next = _i + 1 < _messageHandlerDelegates.Count ? Next : (Func<IMessage, Task>?)null;
+            return _messageHandlerDelegates[_i++](message, next, provider, _token);
+        }
+        private Task Next(IMessage message)
+        {
+            return HandleN(message, _provider!);
+        }
+    }
+}
--- src/MinimalQueues.Core/QueueProcessorHostedService.cs
+++ src/MinimalQueues.Core/QueueProcessorHostedService.cs
@@ -7,8 +7,7 @@ public class QueueProcessorHostedService : IHostedService
 {
     private readonly IServiceScopeFactory _scopeFactory;
     private readonly QueueProcessorOptions _options;
-    private Func<IMessage, IServiceProvider, Task> _handlerDelegate;
-
+    
     public QueueProcessorHostedService(QueueProcessorOptions options, IServiceScopeFactory scopeFactory)
     {
         _scopeFactory = scopeFactory;
@@ -17,14 +16,14 @@ public class QueueProcessorHostedService : IHostedService
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         if (_options.Connection is null) throw new InvalidOperationException("A connection has not been configured.");
-        _handlerDelegate = HandlerDelegateBuilder.Build(_options.MessageHandlerDelegates, cancellationToken);
         await _options.Connection.Start(Processor_ProcessMessageAsync, cancellationToken);
     }
     private async Task Processor_ProcessMessageAsync(IMessage message, CancellationToken cancellationToken)
     {
         using var scope = _scopeFactory.CreateScope();
         var serviceProvider = scope.ServiceProvider;
-        await _handlerDelegate(message, serviceProvider);
+        await new MessageHandlerDelegatesInvoker(_options.MessageHandlerDelegates, cancellationToken)
+            .Handle(message, serviceProvider);
     }
     public async Task StopAsync(CancellationToken cancellationToken)
     {
